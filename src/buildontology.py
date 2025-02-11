171652161efeb56dd3476d9e714973e888eee221
ambroggi <abroggi@umassd.edu>
2025-02-11 15:34:45 -0500

    Split up the big single function
    It is now made of a few smaller functions.

--- src/buildontology.py
+++ src/buildontology.py
@@ -1,6 +1,7 @@
 if __name__ == "__main__":
     import os
     from random import random
+    from typing import Literal
 
     import pandas as pd
     import rdflib
@@ -13,7 +14,7 @@ if __name__ == "__main__":
     NNC = rdflib.Namespace("https://github.com/ambroggi/Pruning-Project-for-Deep-Neural-Networks")   # Neural Network Connections (I guess I should have a location that is not example.org?)
 
 
-def build_base_facts(csv_row: str | int = "0", csv_file: str = "results/BigModel(v0.131).csv", random_=False):
+def get_model_and_datasets(csv_row: str | int = "0", csv_file: str = "results/BigModel(v0.131).csv"):
     config = src.cfg.ConfigObject.get_param_from_args()
     csv_row = str(csv_row)
     config("FromSaveLocation", f"csv {csv_row}" if csv_row.isdigit() else csv_row)
@@ -26,9 +27,139 @@ def build_base_facts(csv_row: str | int = "0", csv_file: str = "results/BigModel
     dataset = dataset.base
     datasets = src.getdata.split_by_class(src.getdata.get_dataloader(load["config"], train), [x for x in range(dataset.number_of_classes)], individual=True, config=load["config"])
     model = src.modelstruct.getModel(load["config"])
+    return model, dataset, datasets
+
+
+def add_layer(g: rdflib.Graph, layer_name: str, layer_index: int, number_of_nodes: int | None = None):
+    layer = rdflib.BNode()
+    g.add((layer, RDF.type, NNC.layer))
+    g.add((layer, RDFS.label, rdflib.Literal(layer_name)))
+    g.add((layer, NNC.layer_index, rdflib.Literal(layer_index)))
+    if number_of_nodes:
+        g.add((layer, NNC.num_nodes, number_of_nodes))
+    return layer
+
+
+def add_node(g: rdflib.Graph, layer: rdflib.Node, node_index: int):
+    n = rdflib.BNode()
+    g.add((n, RDF.type, NNC.node))
+    g.add((n, NNC.layer, layer))
+    g.add((n, NNC.node_index, rdflib.Literal(node_index)))
+    return n
+
+
+def add_meaning(g: rdflib.Graph, node: rdflib.Node, meaning: str, meaning_type: Literal["By Definition", "By Data", "By Inference"] = "By Definition"):
+    meaning_types = {
+        "By Definition": NNC.by_definition,
+        "By Data": NNC.by_data,
+        "By Inference": NNC.by_inference
+    }
+    m = rdflib.BNode()
+    g.add((m, RDF.type, NNC.meaning))
+    g.add((m, NNC.associated_node, node))
+    g.add((node, NNC.meaning, m))
+    g.add((m, NNC.name, rdflib.Literal(meaning)))
+    g.add((m, NNC.mean_type, meaning_types[meaning_type]))
+    return m
+
+
+def add_node_connection(g: rdflib.Graph, output_node: rdflib.Node, weight: torch.Tensor | float, input_node: rdflib.Node):
+    # Note, connections go backwards through the model.
+    # output_node should be on the layer after input_node.
+    w = weight.item() if isinstance(weight, torch.Tensor) else weight
+    c = rdflib.BNode()
+    g.add((c, RDF.type, NNC.connection))
+    g.add((c, NNC.weight, rdflib.Literal(w)))
+    g.add((c, NNC.node, output_node))
+    g.add((c, NNC.connected_to, input_node))
+    return c
+
+
+def setup_input_layer(g: rdflib.Graph, dataset: src.getdata.BaseDataset):
+    input_layer = add_layer(g, "input", -1, number_of_nodes=dataset.number_of_features)
+    dataset_columns = list(dataset.feature_labels.values())
+    last_layer = []
+    for inputNum in range(dataset.number_of_features):
+        # Defining the 'node'
+        i = add_node(g, input_layer, inputNum)
+        g.add((i, RDF.type, NNC.input_node))
+
+        # Giving names to the inputs
+        add_meaning(g, i, dataset_columns[inputNum], "By Definition")
+
+        last_layer.append(i)
+    return last_layer
+
+
+def add_model_layer(g: rdflib.Graph, layer: rdflib.Node, module: torch.nn.modules.Linear, last_layer: list[rdflib.Node], random_: bool):
+    # Define a layer of the network
+    this_layer = []
+    for num, node in enumerate(module.weight):
+        # Define a single node in the layer
+        this_layer.append(add_node(g, layer, num))
+
+        max_connection = None
+        second_max = None
+        for connected_to, connection_weight in enumerate(node):
+            if random_:
+                connection_weight = src.torch.tensor((2*random()) - 1)
+
+            if abs(connection_weight.item()) > 0.001:
+                # Only record connections that are non-zeroed (have a little leeway for pruning methods that use training loop to prune)
+                add_node_connection(g, this_layer[-1], connection_weight, last_layer[connected_to] if len(last_layer) > connected_to else None)
+
+                # Keep track of best 2 connections
+                if max_connection is None or max_connection[0] < abs(connection_weight.item()):
+                    if second_max is None or (max_connection and max_connection[0] > second_max[0]):
+                        second_max = max_connection
+                    max_connection = (abs(connection_weight.item()), connected_to)
+                elif second_max is None or second_max[0] < abs(connection_weight.item()):
+                    second_max = (abs(connection_weight.item()), connected_to)
+
+        # Add primary and secondary contributors
+        if max_connection and len(last_layer) > max_connection[1]:
+            g.add((this_layer[-1], NNC.primary_contributor, last_layer[max_connection[1]]))
+        if second_max and len(last_layer) > second_max[1]:
+            assert last_layer[max_connection[1]] != last_layer[second_max[1]]
+            g.add((this_layer[-1], NNC.secondary_contributor, last_layer[second_max[1]]))
+
+    module.graph_nodes = this_layer
+
+
+def add_model_high_values(g: rdflib.Graph, datasets: list[src.getdata.BaseDataset], model: src.modelstruct.BaseDetectionModel, random_: bool):
+    for class_num, dl in enumerate(datasets):
+        avg_hook = extramodules.Get_Average_Hook()
+        remover = torch.nn.modules.module.register_module_forward_hook(avg_hook)
+        for batch in dl:
+            model(batch[0])
+        for mod_name, module in model.named_modules():
+            if "fc" in mod_name:
+                _, avg = avg_hook.dict[module]
+                if random_:
+                    avg = torch.rand_like(avg)
+                # Add highest node in the layer
+                connection = add_meaning(g, module.graph_nodes[torch.argmax(avg)], "Highest " + datasets[0].base.classes[class_num], "By Data")
+                g.add((connection, NNC.tag, rdflib.Literal(datasets[0].base.classes[class_num])))
+
+                # Add lowest node in the layer
+                connection = add_meaning(g, module.graph_nodes[torch.argmax(-avg)], "Lowest " + datasets[0].base.classes[class_num], "By Data")
+                g.add((connection, NNC.tag, rdflib.Literal(datasets[0].base.classes[class_num])))
+
+                # Remove highest
+                avg[torch.argmax(avg)] = torch.min(avg)
+
+                # Add second highest node in the layer (by virtue of the highest having been removed)
+                connection = add_meaning(g, module.graph_nodes[torch.argmax(avg)], "Second " + datasets[0].base.classes[class_num], "By Data")
+                g.add((connection, NNC.tag, rdflib.Literal(datasets[0].base.classes[class_num])))
+        remover.remove()
+        del avg_hook
+
+
+def build_base_facts(csv_row: str | int = "0", csv_file: str = "results/BigModel(v0.131).csv", random_=False):
+    model, dataset, datasets = get_model_and_datasets(csv_row, csv_file)
+
     layer = None
     last_layer = []
-    this_layer = []
 
     if not random_:
         filename = f"datasets/model({csv_file.split('/')[-1]} {csv_row}).ttl"
@@ -41,115 +172,20 @@ def build_base_facts(csv_row: str | int = "0", csv_file: str = "results/BigModel
 
     count = 0
 
-    # Define the input vector layer of the network
-    input_layer = rdflib.BNode()
-    g.add((input_layer, RDF.type, NNC.layer))
-    g.add((input_layer, RDFS.label, rdflib.Literal("Input")))
-    g.add((input_layer, NNC.layer_index, rdflib.Literal(-1)))
+    # setup for the initial input values to relate the first layer to the input vector
+    last_layer = setup_input_layer(g, dataset)
 
     for mod_name, module in model.named_modules():
         if "fc" in mod_name:
-            # Define a layer of the network
-            layer = rdflib.BNode()
-            g.add((layer, RDF.type, NNC.layer))
-            g.add((layer, RDFS.label, rdflib.Literal(mod_name)))
-            g.add((layer, NNC.layer_index, rdflib.Literal(count)))
-            g.add((layer, NNC.num_nodes, rdflib.Literal(len(module.weight))))
-            for num, node in enumerate(module.weight):
-                # Define a single node in the layer
-                n = rdflib.BNode()
-                this_layer.append(n)
-                g.add((n, RDF.type, NNC.node))
-                g.add((n, NNC.layer, layer))
-                g.add((n, NNC.node_index, rdflib.Literal(num)))
-
-                # setup for the initial input values to relate the first layer to the input vector
-                if len(last_layer) == 0:
-                    dataset_columns = list(dataset.feature_labels.values())
-                    for inputNum, _ in enumerate(node):
-                        # Defining the 'node'
-                        i = rdflib.BNode()
-                        g.add((i, RDF.type, NNC.node))
-                        g.add((i, RDF.type, NNC.input_node))
-                        g.add((i, NNC.layer, input_layer))
-                        g.add((i, NNC.node_index, rdflib.Literal(inputNum)))
-
-                        # Giving names to the inputs
-                        m = rdflib.BNode()
-                        g.add((m, RDF.type, NNC.meaning))
-                        g.add((m, NNC.associated_node, i))
-                        g.add((i, NNC.meaning, m))
-                        g.add((m, NNC.name, rdflib.Literal(dataset_columns[inputNum])))
-
-                        last_layer.append(i)
-                    g.add((input_layer, NNC.num_nodes, rdflib.Literal(len(node))))
-
-                max_connection = None
-                second_max = None
-                for connected_to, connec in enumerate(node):
-                    if random_:
-                        connec = src.torch.tensor((2*random()) - 1)
-                    if abs(connec.item()) > 0.001:
-                        # if connec.item() > 0.001:
-                        c = rdflib.BNode()
-                        g.add((c, RDF.type, NNC.connection))
-                        g.add((c, NNC.weight, rdflib.Literal(connec.item())))
-                        g.add((c, NNC.node, n))
-
-                        if len(last_layer) > connected_to:
-                            g.add((c, NNC.connected_to, last_layer[connected_to]))
-
-                        if max_connection is None or max_connection[0] < abs(connec.item()):
-                            if second_max is None or (max_connection and max_connection[0] > second_max[0]):
-                                second_max = max_connection
-                            max_connection = (abs(connec.item()), connected_to)
-                        elif second_max is None or second_max[0] < abs(connec.item()):
-                            second_max = (abs(connec.item()), connected_to)
-
-                        # if max_connection is None or max_connection[0] < connec.item():
-                        #     max_connection = (connec.item(), connected_to)
-
-                if max_connection and len(last_layer) > max_connection[1]:
-                    g.add((n, NNC.primary_contributor, last_layer[max_connection[1]]))
-                if second_max and len(last_layer) > second_max[1]:
-                    assert last_layer[max_connection[1]] != last_layer[second_max[1]]
-                    g.add((n, NNC.secondary_contributor, last_layer[second_max[1]]))
-
-            module.graph_nodes = this_layer
-            last_layer = this_layer
-            this_layer = []
+            layer = add_layer(g, mod_name, count, number_of_nodes=len(module.weight))
+            add_model_layer(g, layer, module, last_layer, random_)
+            last_layer = module.graph_nodes
             count += 1
 
     for attack_type, associated_final_node in enumerate(last_layer):
-        atk = rdflib.BNode()
-        g.add((atk, RDF.type, NNC.meaning))
-        g.add((atk, NNC.associated_node, associated_final_node))
-        g.add((associated_final_node, NNC.meaning, atk))
-        g.add((atk, NNC.name, rdflib.Literal(dataset.classes[attack_type])))
+        add_meaning(g, associated_final_node, dataset.classes[attack_type], "By Definition")
 
-    for class_num, dl in enumerate(datasets):
-        avghook = extramodules.Get_Average_Hook()
-        remover = torch.nn.modules.module.register_module_forward_hook(avghook)
-        for batch in dl:
-            model(batch[0])
-        for mod_name, module in model.named_modules():
-            if "fc" in mod_name:
-                _, avg = avghook.dict[module]
-                if random_:
-                    avg = torch.rand_like(avg)
-                connection = rdflib.BNode()
-                g.add((connection, RDF.type, NNC.meaning))
-                g.add((connection, NNC.associated_node, module.graph_nodes[torch.argmax(avg)]))
-                g.add((connection, NNC.name, rdflib.Literal("Highest " + dataset.classes[class_num])))
-                g.add((connection, NNC.tag, rdflib.Literal(dataset.classes[class_num])))
-                avg[torch.argmax(avg)] = torch.min(avg)
-                connection = rdflib.BNode()
-                g.add((connection, RDF.type, NNC.meaning))
-                g.add((connection, NNC.associated_node, module.graph_nodes[torch.argmax(avg)]))
-                g.add((connection, NNC.name, rdflib.Literal("Second  " + dataset.classes[class_num])))
-                g.add((connection, NNC.tag, rdflib.Literal(dataset.classes[class_num])))
-        remover.remove()
-        del avghook
+    add_model_high_values(g=g, datasets=datasets, model=model, random_=random_)
 
     g.serialize(filename, encoding="UTF-8")
     return filename, g
@@ -252,9 +288,19 @@ def make_pivot_table_from_top_down_connections():
     return table
 
 
+def select_best_rows(csv_file: str = "results/BigModel(v0.131).csv"):
+    # Wanted to make this automatic but did not eventually do that.
+    df = pd.read_csv("results/BigModel(v0.131).csv")
+    df = df[df["WeightPrunePercent"] == "[0.62, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 0.56, 1.0]"]
+    df = df[df["Notes"] == 0]
+    return list(df.index)
+
+
 if __name__ == "__main__":
     if not False:
         for x in [0, 1, 2]:
             if not os.path.exists(f"datasets/model(BigModel(v0.131).csv {x}).ttl"):
                 path, g = build_base_facts(random_=False, csv_row=f"{x}")
             run_really_long_query(f"datasets/model(BigModel(v0.131).csv {x}).ttl")
+    else:
+        print(select_best_rows())
